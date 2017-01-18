25a09589475741e01dabe897b646790cdef0db95
amank37 <amanc0682337@gmail.com>
2017-01-18 11:26:47 -0500

    Initial Commit

---
+++ src/csd4464/assignment1/techfiree/CSD4464Assignment1TechFirEE.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.assignment1.techfiree;
+
+/**
+ *
+ * @author c0682337
+ */
+public class CSD4464Assignment1TechFirEE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
